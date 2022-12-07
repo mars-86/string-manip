c0ae8feb8b34a6b7caacdf0c7a77f54a054d5fc0
mars-86 <hrslash86@gmail.com>
2022-12-06 21:50:31 -0300

    fix: str_lowercase and str_uppercase

--- lib/str_lowercase.c
+++ lib/str_lowercase.c
@@ -4,17 +4,19 @@
 
 char *str_lowercase(char *dest, const char *src)
 {
+    char *__dest = dest;
     while (*src != '\0')
-        *dest++ = TEST_UPPERCASE(*src) ? TO_LOWERCASE(*src++) : *src++;
-    *dest = '\0';
+        *__dest++ = TEST_UPPERCASE(*src) ? TO_LOWERCASE(*src) : *src, src++;
+    *__dest = '\0';
     return dest;
 }
 
 char *strn_lowercase(char *dest, const char *src, size_t n)
 {
+    char *__dest = dest;
     int __i = 0;
     while (*src != '\0')
-        *dest++ = (__i++ < n && TEST_UPPERCASE(*src)) ? TO_LOWERCASE(*src++) : *src++;
-    *dest = '\0';
+        *__dest++ = (__i++ < n && TEST_UPPERCASE(*src)) ? TO_LOWERCASE(*src++) : *src++;
+    *__dest = '\0';
     return dest;
 }
